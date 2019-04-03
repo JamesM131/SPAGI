246f5942266d38a23aa99580081d4275d807e97c
James Monks <jamesmonks131@gmail.com>
2019-04-03 18:11:29 +1100

    Add test infrastructure

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(spagi)
+
+test_check("spagi")
---
+++ tests/testthat/test-k-paths.R
@@ -0,0 +1,13 @@
+context("test-k-paths")
+
+
+
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+
+
+test_that("path is a {{ ??list?? }}", {
+  
+})
