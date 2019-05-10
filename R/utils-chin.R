896723c851ff40bd49b249a0f0bfb6d8b951e240
James Monks <jamesmonks131@gmail.com>
2019-05-10 12:09:27 +1000

    Add %chin% to function imports (not sure if this works)

---
+++ R/utils-chin.R
@@ -0,0 +1,11 @@
+#' %chin% 
+#'
+#' See \code{data.table::\link[data.table]{\%chin\%}} for details.
+#'
+#' @name %chin%
+#' @rdname chmatch
+#' @keywords internal
+#' @export
+#' @importFrom data.table %chin%
+#' @usage x \%chin\% table
+NULL
